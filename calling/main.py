523537d991434098c333d41717965a130f13fd7b
Wouter van der Wal <wouter.vdwal@outlook.com>
2025-08-08 10:20:53 +0200

    Update main.py

--- calling/main.py
+++ calling/main.py
@@ -198,4 +198,4 @@ def generate_opening_message():
     return audio_file
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=80, debug=True)
+    app.run(host="0.0.0.0", port=8000, debug=True)
